25041483b791fb232516adeca7d5d15e49c82135
SapanKr800 <sapankr800@gmail.com>
2026-01-01 23:24:18 +0530

    first commit

--- api/news.js
+++ api/news.js
@@ -1,18 +1,16 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
-  const { category = "business", q } = req.query;
+  const { category = "general", q } = req.query;
   const API_KEY = process.env.VITE_API_KEY;
 
   try {
     let url = "";
 
     if (q) {
-      // 🔍 Search
-      url = `https://newsapi.org/v2/everything?q=${q}&language=en&apiKey=${API_KEY}`;
+      url = `https://gnews.io/api/v4/search?q=${q}&lang=en&country=in&token=${API_KEY}`;
     } else {
-      // 📰 Category news
-      url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${API_KEY}`;
+      url = `https://gnews.io/api/v4/top-headlines?category=${category}&lang=en&country=in&token=${API_KEY}`;
     }
 
     const response = await axios.get(url);
@@ -20,8 +18,8 @@ export default async function handler(req, res) {
     res.status(200).json({
       articles: response.data.articles || [],
     });
-  } catch (error) {
-    console.error(error.response?.data || error.message);
+  } catch (err) {
+    console.error(err.response?.data || err.message);
     res.status(500).json({ articles: [] });
   }
 }
